18b495584705267ef360c07269cbf8468057da81
shai <grysiewiczszymon47@gmail.com>
2025-11-15 21:44:25 +0100

    szymon/register

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -3,12 +3,11 @@ import Link from "next/link";
 
 export default function RegisterPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className=" text-2xl font-bold font-geologica">Stick With It!!!</h1>
-      <div className="text-center space-y-6">
-        <h1 className="text-4xl font-bold">Register</h1>
+    <div className="min-h-screen flex flex-col gap-16 items-center justify-center">
+      <h1 className=" text-3xl font-bold font-geologica">Stick With It!!!</h1>
+      <div className="text-center ">
         <RegisterForm />
-        <p className="text-sm">
+        <p className="text-sm mt-2">
           Already have an account?{" "}
           <Link href="/login" className="text-blue-500 hover:underline">
             Login
