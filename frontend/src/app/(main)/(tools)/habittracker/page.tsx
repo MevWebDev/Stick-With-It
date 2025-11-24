ce6218663ae457c3d7c17ef0b3f9a62ab7c57141
shai <grysiewiczszymon47@gmail.com>
2025-11-24 17:51:59 +0100

    Tools menu

---
+++ frontend/src/app/(main)/(tools)/habittracker/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import HabitTracker from "@/app/components/HabitTracker/HabitTracker";
+
+export default function HabitTrackerPage() {
+  return (
+    <>
+      <HabitTracker />
+    </>
+  );
+}
---
+++ frontend/src/app/(main)/(tools)/pomodoro/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PomodoroTimer from "@/app/components/PomodoroTimer/pomodoroTimer";
+
+export default function HabitTrackerPage() {
+  return (
+    <>
+      <PomodoroTimer />
+    </>
+  );
+}
