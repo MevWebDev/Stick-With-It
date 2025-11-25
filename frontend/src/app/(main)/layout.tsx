33abc2513ca544498e4d463f61829bc4afe7d198
MevWebDev <szymongrysiewicz@gmail.com>
2025-11-25 01:03:06 +0100

    Fixed layout so the bottom navbar wont go down if content is taller than page

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -7,9 +7,9 @@ export default function MainLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-screen flex flex-col">
+    <div className="h-[100dvh] flex flex-col">
       <Topbar />
-      <div className="flex-grow">{children}</div>
+      <div className="flex-grow overflow-y-auto">{children}</div>
       <Navbar />
     </div>
   );
