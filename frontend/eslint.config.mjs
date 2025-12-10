545d5457d75d0afdeaa9b990ba1b106a2f42b685
MevWebDev <szymongrysiewicz@gmail.com>
2025-12-10 23:08:35 +0100

    removed uneccessary imports, variables

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -23,6 +23,14 @@ const eslintConfig = [
   {
     rules: {
       "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
     },
   },
 ];
