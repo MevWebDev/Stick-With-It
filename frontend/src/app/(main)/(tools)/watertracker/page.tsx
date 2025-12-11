b93bda3d647addb8b9bb87f10e9ced47b34c3df9
KondradKolon <grzegorzalwasiak@gmail.com>
2025-12-11 01:14:08 +0100

    added emojis and water tracker

---
+++ frontend/src/app/(main)/(tools)/watertracker/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import WaterTracker from "@/app/components/WaterTracker/WaterTracker";
+
+export default function WaterTrackerPage() {
+  return (
+    <>
+      <WaterTracker />
+    </>
+  );
+}
