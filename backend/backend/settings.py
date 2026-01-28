8a91350535397315b01a72f4adea00bad5a70151
MevWebDev <szymongrysiewicz@gmail.com>
2026-01-28 23:52:40 +0100

    okay I am preatty sure it will work rn

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-mlc@tkg4$lm_je
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,upbeat-patience-production.up.railway.app').split(',')
 
 
 # Application definition
