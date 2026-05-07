f75e77ec957d4e493d7ef31c04c6d6d1f7411740
Grzegorz Alwasiak <148322868+KondradKolon@users.noreply.github.com>
2026-05-07 17:20:56 +0200

    trying to fix
    https://github.com/shadowwalker/next-pwa/issues/295

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
-
+const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
+  runtimeCaching, 
+  buildExcludes: [/middleware-manifest\.json$/],
 });
 
 const nextConfig: NextConfig = {
